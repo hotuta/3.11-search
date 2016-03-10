593b3d044da8d5ff04cd9e0d774cac03c81c5b97
hotuta <8985703@gmail.com>
2016-03-11 01:19:53 +0900

    永遠に検索できるように修正

--- 2016.3.11_search.rb
+++ 2016.3.11_search.rb
@@ -1,9 +1,11 @@
 require 'rubygems'
 require 'selenium-webdriver'
-wd = Selenium::WebDriver.for :chrome
 
-wd.get "http://fukko.yahoo.co.jp/"
-sleep 2
-wd.find_element(:id, "search_btn").click
-sleep 2
-wd.quit
+loop do
+  wd = Selenium::WebDriver.for :chrome
+  wd.get "http://fukko.yahoo.co.jp/"
+  sleep 2
+  wd.find_element(:id, "search_btn").click
+  sleep 2
+  wd.quit
+end
